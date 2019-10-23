9722bbc2d77c95e1e9c55de3c76318e516b8d635
cweo <43748723+cweo@users.noreply.github.com>
2019-10-23 14:54:34 +0200

    cleaning the python files, add documentation for each functions

--- implementations.py
+++ implementations.py
@@ -1,56 +1,16 @@
 import numpy as np
 from proj1_helpers import *
 import matplotlib.pyplot as plt
-# Preprocessing functions
 
 
-def split_data(x, y, ratio, seed=1):
+def build_k_indices(y, k_num, seed=1):
     """
-    split the dataset based on the split ratio. If ratio is 0.8 
-    you will have 80% of your data set dedicated to training 
-    and the rest dedicated to testing
+    Build k indices for k-fold.
+    Arguments: 
+    - y: the ground truth matrix
+    - k_num: the number of folds
+    - seed: the seed used during the pseudo-random shuffle, use the same seed for the same output
     """
-    # set seed
-    np.random.seed(seed)
-    ids = [i for i in range(len(x))]
-    train_ids = np.random.choice(ids, int(ratio*len(ids)))
-    test_ids = []
-    for i in range(len(ids)):
-        if not(i in train_ids):
-            test_ids.append(i)
-
-    return x[train_ids], y[train_ids], x[test_ids], y[test_ids]
-
-
-def dataset_minmax(tx):
-    minmax = list()
-    for i in range(len(tx[0])):
-        col_values = [row[i] for row in tx]
-        value_min = min(col_values)
-        value_max = max(col_values)
-        minmax.append([value_min, value_max])
-    return minmax
-
-def standardize(x):
-    """Standardize the original data set."""
-    mean_x = np.mean(x, axis=0)
-    x = x - mean_x
-    std_x = np.std(x, axis=0)
-    x = x / std_x
-    return x, mean_x, std_x
-
-# Rescale dataset columns to the range 0-1
-
-
-def normalize_dataset(tx, minmax):
-    for row in tx:
-        for i in range(len(row)):
-            if((minmax[i][1] - minmax[i][0])!=0):
-                row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
-                          
-    
-def build_k_indices(y, k_num, seed):
-    """build k indices for k-fold."""
     num_row = y.shape[0]
     interval = int(num_row / k_num)
     np.random.seed(seed)
@@ -102,24 +62,49 @@ def cross_validation(y, x, k_indices, num_k, lambda_, degree):
 
 # Loss functions
 
-
-def compute_loss(y, tx, w):
-
+def compute_mse(y, tx, w):
+    """
+    Compute the mean square error of the predicted output compared to the ground truth results.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the linear model
+    """
     N = y.shape[0]
     e = y - np.dot(tx, w)
     L = (1/(2*N))*np.dot(e.transpose(), e)
     return L
 
-
 def compute_rmse(y, tx, w):
-    return np.sqrt(2*compute_loss(y, tx, w))
+    """
+    Compute the root mean square error of the predicted output compared to the ground truth results.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the linear model
+    """
+    return np.sqrt(2*compute_mse(y, tx, w))
 
-def sigmoid(t):
-    """apply sigmoid function on t."""
-    return 1.0/(1+np.exp(-t))
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
+    
+def sigmoid_deriv(x):
+    return x * (1 - x)
+
+def relu(x):
+    if x < 0:
+        return 0
+    else:
+        return x
 
 def compute_loss_log_reg(y, tx, w):
-    """compute the cost by negative log likelihood."""
+    """
+    Compute the cost by negative log likelihood.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the model
+    """
     N = y.shape[0]
     loss = 0
     for n in range(N):
@@ -127,22 +112,52 @@ def compute_loss_log_reg(y, tx, w):
         loss += -(y[n]*np.log(sigmoid_value)+(1-y[n])*np.log(1-sigmoid_value))
     return loss
 
+def compute_loss_reg_log_reg(y, tx, w, lambda_):
+    """
+    Compute the cost by negative log likelihood with a ridge term.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the model
+    """
+    loss = compute_loss_log_reg(y,tx,w) + lambda_/2*np.dot(w.T,w)
+    return loss
+
 # Gradient functions
 
-def compute_gradient(y, tx, w):
+def compute_gradient_mse(y, tx, w):
+    """
+    Compute gradient in mse context.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the linear model
+    """
     N = y.size
     return -np.dot(np.transpose(tx), y-np.dot(tx, w))/N
 
 
 def compute_gradient_log_reg(y, tx, w):
-    """compute the gradient of loss."""
+    """"
+    Compute gradient in logistic regression context.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the model
+    """
     sigmoid_value = sigmoid(np.dot(tx,w))
     return np.dot(tx.T,sigmoid_value-y)
 
 # Hessian functions
 
 def compute_hessian_log_reg(y, tx, w):
-    """return the hessian of the loss function."""
+    """
+    Compute hessian in logistic regression context.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the model
+    """
     N = y.shape[0]
     hessian_diag = np.zeros(N)
     for n in range(N):
@@ -151,209 +166,157 @@ def compute_hessian_log_reg(y, tx, w):
     S = np.diag(hessian_diag)
     return np.dot(tx.T,np.dot(S,tx))
 
+# All inclusive functions for loss, gradient and hessian
 
-
-
-# Regression functions
-
-def logistic_regression(y, tx, w):
-    """return the loss, gradient, and hessian."""
+def compute_log_reg(y, tx, w):
+    """
+    Compute loss, gradient and hessian in logistic regression context.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the model
+    """
     loss = compute_loss_log_reg(y, tx, w)
     gradient = compute_gradient_log_reg(y, tx, w)
     hessian = compute_hessian_log_reg(y, tx, w)
     return loss, gradient, hessian
 
-def penalized_logistic_regression(y, tx, w, lambda_):
-    """return the loss, gradient, and hessian."""
+def compute_pen_log_reg(y, tx, w, lambda_):
+    """
+    Compute loss, gradient and hessian in penalized logistic regression context.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - w: weights for the model
+    """
     loss = compute_loss_log_reg(y,tx,w) + lambda_/2*np.dot(w.T,w)
     gradient = compute_gradient_log_reg(y,tx,w) + lambda_*w
-    #hessian = compute_hessian_log_reg(y,tx,w) + lambda_
-    return loss, gradient#, hessian
+    hessian = compute_hessian_log_reg(y,tx,w) + lambda_
+    return loss, gradient, hessian
 
 
-def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+# Regression functions
+
+def least_squares_GD(y, tx, initial_w, max_iters=1000, gamma=0.001):
+    """
+    Linear regression using gradient descent.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - initial_w: initial weights for the model
+    - max_iters: maximum number of iterations during the gradient descent, default: 1000
+    - gamma: the learning rate, default: 0.001
+    """
     ws = initial_w
     N = y.shape[0]
-
     for i in range(max_iters):
-
-        grad = compute_gradient(y, tx, ws)
+        grad = compute_gradient_mse(y, tx, ws)
         ws = ws - gamma*grad
-
-    loss = compute_loss(y, tx, ws)
-
+    loss = compute_mse(y, tx, ws)
     return loss, ws
 
-
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-
+def least_squares_SGD(y, tx, initial_w, max_iters=1000, gamma=0.001):
+    """
+    Linear regression using stochastic gradient descent with mini-batch size 1.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - initial_w: initial weights for the model
+    - max_iters: maximum number of iterations during the gradient descent, default: 1000
+    - gamma: the learning rate, default: 0.001
+    """
     ws = initial_w
     N = y.shape[0]
-
     for i in range(max_iters):
         for n in range(N):
-            grad = compute_gradient(y[n], tx[n], ws)
+            grad = compute_gradient_mse(y[n], tx[n], ws)
             ws = ws - gamma*grad
-
-    loss = compute_loss(y, tx, ws)
-
+    loss = compute_mse(y, tx, ws)
     return loss, ws
-
 
 def least_squares(y, tx):
-
+    """
+    Linear regression using normal equations. Be sure to have a non singular matrix.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    """
     Gram = np.dot(np.transpose(tx), tx)
-
     ws = np.dot(np.linalg.inv(Gram), np.dot(np.transpose(tx), y))
-
-    loss = compute_loss(y, tx, ws)
-
+    loss = compute_mse(y, tx, ws)
     return loss, ws
-
-
-def least_squares_rmse(y, tx):
-
-    Gram = np.dot(np.transpose(tx), tx)
-
-    ws = np.dot(np.linalg.inv(Gram), np.dot(np.transpose(tx), y))
-
-    loss = compute_rmse(y, tx, ws)
-
-    return loss, ws
-
 
 def ridge_regression(y, tx, lambda_):
-    """implement ridge regression."""
+    """
+    Ridge regression using normal equations. Be sure to have a non singular matrix.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - lambda_: the penalizing parameter
+    """
     Gram = np.dot(np.transpose(tx), tx)
     reg_matrix = lambda_/(2*Gram.shape[0])*np.identity(Gram.shape[0])
     ws = np.dot(np.linalg.inv(Gram+reg_matrix), np.dot(np.transpose(tx), y))
-    loss = compute_rmse(y, tx, ws)
+    loss = compute_mse(y, tx, ws)
     return loss, ws
 
-def build_poly(x, degree):
-    """Build polynomial features for input data x."""
-    # 0 to degree
-    phi_x = np.ones((len(x), 1))
-    for deg in range(1, degree+1):
-        phi_x = np.c_[phi_x, np.power(x, deg)]
-    # square root of absolute value
-    phi_x = np.c_[phi_x, np.sqrt(np.abs(x))]
-    # cross terms
-    # vectorize the calculation
-    # ref: https://stackoverflow.com/questions/22041561/python-all-possible-products-between-columns
-    i, j = np.triu_indices(x.shape[1], 1)
-    phi_x = np.c_[phi_x, x[:, i] * x[:, j]]
-    return phi_x
-
-
-def polynomial_regression(y, x, degrees):
-    """Constructing the polynomial basis function expansion of the data,
-       and then running least squares regression."""
-    # define parameters
-    # degrees = [1, 3, 7, 12]
-
-    for ind, degree in enumerate(degrees):
-        phi_x = build_poly(x, degree)
-        loss, ws = least_squares_rmse(y, phi_x)
+def logistic_regression(y, tx, initial_w, max_iters=1000, gamma=0.001):
+    """
+    Logistic regression using stochastic gradient descent with mini-batch size 1.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - initial_w: initial weights for the model
+    - max_iters: maximum number of iterations during the gradient descent, default: 1000
+    - gamma: the learning rate, default: 0.001
+    """
+    ws = initial_w
+    N = y.shape[0]
+    for i in range(max_iters):
+        for n in range(N):
+            grad = compute_gradient_log_reg(y[n], tx[n], ws)
+            ws = ws - gamma*grad
+    loss = compute_loss_log_reg(y, tx, ws)
     return loss, ws
 
-# One step gradient descent
-
-def learning_by_gradient_descent(y, tx, w, gamma):
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters=1000, gamma=0.001):
     """
-    Do one step of gradient descen using logistic regression.
-    Return the loss and the updated w.
+    Penalized logistic regression using stochastic gradient descent with mini-batch size 1.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - lambda_: the penalizing parameter
+    - initial_w: initial weights for the model
+    - max_iters: maximum number of iterations during the gradient descent, default: 1000
+    - gamma: the learning rate, default: 0.001
     """
-    loss = compute_loss_log_reg(y, tx, w)
-    gradient = compute_gradient_log_reg(y, tx, w)
-    w = w - gamma*gradient
-    return loss, w
+    ws = initial_w
+    N = y.shape[0]
+    for i in range(max_iters):
+        for n in range(N):
+            grad = compute_gradient_log_reg(y,tx,ws) + lambda_*ws
+            ws = ws - gamma*grad
+    loss = compute_loss_reg_log_reg(y, tx, ws)
+    return loss, ws
 
-def learning_by_newton_method(y, tx, w, gamma):
+def reg_logistic_regression_newton(y, tx, lambda_, initial_w, max_iters=1000, gamma=0.001):
     """
-    Do one step on Newton's method.
-    return the loss and updated w.
+    Penalized logistic regression using newton method.
+    Arguments:
+    - y: matrix of ground truth results
+    - tx: features matrix
+    - lambda_: the penalizing parameter
+    - initial_w: initial weights for the model
+    - max_iters: maximum number of iterations during the gradient descent, default: 1000
+    - gamma: the learning rate, default: 0.001
     """
-    loss, gradient, hessian = logistic_regression(y, tx, w)
-    w = w - gamma*np.dot(np.linalg.inv(hessian),gradient)
-    return loss, w
-
-def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
-    """
-    Do one step of gradient descent, using the penalized logistic regression.
-    Return the loss and updated w.
-    """
-    loss, gradient= penalized_logistic_regression(y, tx, w, lambda_)#, hessian 
-    w = w - gamma*gradient
-    return loss, w
-
-
-
-# Metrics : accuracy, f1_score
-# https://en.wikipedia.org/wiki/Precision_and_recall
-def metrics(weights, y_test, x_test, predict=predict_labels):
-    tp, fp, tn, fn = 0, 0, 0, 0
-    y_pred = predict(weights, x_test)
-    for i in range(len(y_pred)):
-        if (y_pred[i] == 1):
-            if (y_test[i] == 1):
-                tp += 1
-            else:
-                fp += 1
-        else:
-            if (y_test[i] == 1):
-                fn += 1
-            else:
-                tn += 1
-    #precision = tp/(tp+fp)
-    #recall = tp/(tp+fn)
-    accuracy = (tp+tn)/(tp+tn+fp+fn)
-    f1_score = 2*tp/(2*tp+tn+fp+fn)
-
-    return accuracy, f1_score
-
-def metrics_check(y_test, y_pred):
-    tp, fp, tn, fn = 0, 0, 0, 0
-    for i in range(len(y_pred)):
-        if (y_pred[i] == 1):
-            if (y_test[i] == 1):
-                tp += 1
-            else:
-                fp += 1
-        else:
-            if (y_test[i] == 1):
-                fn += 1
-            else:
-                tn += 1
-    #precision = tp/(tp+fp)
-    #recall = tp/(tp+fn)
-    accuracy = (tp+tn)/(tp+tn+fp+fn)
-    f1_score = 2*tp/(2*tp+tn+fp+fn)
-
-    return accuracy, f1_score
-
-
-# PostProcessing functions
-def plot_acc_f1(cat_acc, f1_score, lambdas):
-    """
-    train_errors, test_errors and lambas should be list (of the same size) the respective train error and test error for a given lambda,
-    * lambda[0] = 1
-    * train_errors[0] = RMSE of a ridge regression on the train set
-    * test_errors[0] = RMSE of the parameter found by ridge regression applied on the test set
-
-    degree is just used for the title of the plot.
-    """
-    plt.semilogx(lambdas, cat_acc, color='b', marker='*',
-                 label="Categorical accuracy")
-    plt.semilogx(lambdas, f1_score, color='r', marker='*', label="f1 score")
-    plt.xlabel("lambda")
-    plt.ylabel("Value")
-    plt.title("Ridge regression")
-    leg = plt.legend(loc=1, shadow=True)
-    leg.draw_frame(False)
-    plt.savefig("ridge_regression")
+    ws = initial_w
+    N = y.shape[0]
+    for i in range(max_iters):
+        loss, gradient, hessian = compute_pen_log_reg(y, tx, ws, lambda_)
+        ws = ws - gamma*np.dot(np.linalg.inv(hessian),gradient)
+    return loss, ws
     
-
 # Neural Network (1 weighter layer, 1 bias layer, activation layer)
 def create_neural_network(layer_array, input_dims):
     weights = []
@@ -381,19 +344,6 @@ def create_neural_network(layer_array, input_dims):
         activations.append(layer_array[i][1])
     return [weights, biases, activations]
 
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-    
-def sigmoid_deriv(x):
-    return x * (1 - x)
-
-def relu(x):
-    if x < 0:
-        return 0
-    else:
-        return x
-    
     
 def predict_ratio(data, neural_net):
     weights = neural_net[0]
--- proj1_helpers.py
+++ proj1_helpers.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-"""some helper functions for project 1."""
 import csv
 import numpy as np
 import implementations
 import copy
 
+""" Loading the data tool """
+
 def load_csv_data(data_path, sub_sample=False):
-    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    """
+    Loads data and returns y (class labels), tX (features) and ids (event ids)
+    Arguments:
+    - data_path: the path of the csv file
+    - sub_sample: whether we should sub_sample or not, default: False
+    """
     y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
     x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
     ids = x[:, 0].astype(np.int)
@@ -24,6 +30,129 @@ def load_csv_data(data_path, sub_sample=False):
 
     return yb, input_data, ids
 
+            
+""" Preprocessing tools""" 
+
+def split_data(x, y, ratio=0.8, seed=1):
+    """
+    Split the dataset based on the split ratio. If ratio is 0.8 you will have 80% of your data set dedicated to training and the rest dedicated to testing.
+    Arguments:
+    - y: the column of ground truth results
+    - x: the features matrix
+    - ratio: the split ratio between 0 and 1, default: 0.8
+    - seed: the seed used during the pseudo-random shuffle, use the same seed for the same output
+    """
+    # Set the seed
+    np.random.seed(seed)
+    ids = [i for i in range(len(x))]
+    train_ids = np.random.choice(ids, int(ratio*len(ids)))
+    test_ids = []
+    for i in range(len(ids)):
+        if not(i in train_ids):
+            test_ids.append(i)
+
+    return x[train_ids], y[train_ids], x[test_ids], y[test_ids]
+
+
+def standardize(x):
+    """
+    Standardize the original data set x. You should make a deepcopy before computing this function.
+    Argument : x: the features matrix
+    """
+    mean_x = np.mean(x, axis=0)
+    x = x - mean_x
+    std_x = np.std(x, axis=0)
+    x = x / std_x
+    return x, mean_x, std_x
+    
+
+def preprocessing(y,tx, train = True, columns_to_keep = None):    
+    """
+    Return a cleaned dataset (y,tx), operations on it depends whether it's a training dataset or a testing dataset
+    Arguments:
+    - y: the column to be predicted, can be None if in testing mode
+    - tx: the features matrix
+    - train: whether we are in training mode or testing mode, default: training mode
+    - columns_to_keep: required if we are in testing mode
+    """
+    
+    def clean_xi(xi, train, columns_to_keep = None):
+        """ Function to clean in depth each categorical dataset """
+        x = copy.deepcopy(xi)
+        # Set the -999 value to the median of the column
+        x = np.where(x==-999, np.nan, x)
+        col_median = np.nanmedian(x, axis=0)
+        inds_nan = np.where(np.isnan(x))
+        x[inds_nan] = np.take(col_median, inds_nan[1])
+        
+        if train:
+            # Remove the Nan and constant columns
+            nan_columns = np.all(np.isnan(x), axis=0)
+            cst_columns = np.all(x == x[0,:], axis = 0)
+            columns_to_remove = nan_columns | cst_columns
+            x = x[:,~columns_to_remove]
+            # Standardize the features         
+            x,_,_ = implementations.standardize(x)
+            # Insert a 1 column for a non null intersection
+            x = np.insert(x, 0, 1, axis=1)
+            return x, columns_to_remove
+        else:  
+            # Remove the same columns that were removed during the preprocessing of the training phase   
+            x = x[:,columns_to_keep]
+            # Standardize the features  
+            x,_,_ = implementations.standardize(x)
+            # Insert a 1 column for non null intersection
+            x = np.insert(x, 0, 1, axis=1)
+            return x
+
+    # Split the rows base on categories of 22th column of features which is the number of jets       
+    rows0 = tx[:,22]==0
+    rows1 = tx[:,22]==1
+    rows2 = np.logical_or(tx[:,22]==2, tx[:,22]==3)
+
+    if train:
+        # Split the training dataset 
+        y0, x0 = y[rows0], tx[np.where(rows0)]
+        y1, x1 = y[rows1], tx[np.where(rows1)]
+        y2, x2 = y[rows2], tx[np.where(rows2)]
+        # Clean the training dataset with the nested function and collect removed columns
+        x0, columns_to_remove0 = clean_xi(x0, train)
+        x1, columns_to_remove1 = clean_xi(x1, train)
+        x2, columns_to_remove2 = clean_xi(x2, train)
+        return [(y0,x0),(y1,x1),(y2,x2), (~columns_to_remove0,~columns_to_remove1,~columns_to_remove2)]
+    else:
+        # Get back the columns that should be kept in the testing dataset
+        i0, i1, i2 = columns_to_keep
+        # Split the testing dataset
+        x0 = tx[np.where(rows0)]        
+        x1 = tx[np.where(rows1)]        
+        x2 = tx[np.where(rows2)]
+        # Clean the testing dataset with the nested function using the columns that were remove during the training phase
+        x0 = clean_xi(x0, train, i0)
+        x1 = clean_xi(x1, train, i1)
+        x2 = clean_xi(x2, train, i2)
+        return [x0, x1, x2, rows0, rows1, rows2]
+
+def build_poly(x, degree):
+    """
+    Build polynomial features for input data x with the degree.
+    Arguments:
+    - x: features matrix
+    - degree: the degree of the polynom used for data augmentation
+    """
+    phi_x = np.ones((len(x), 1))
+    for deg in range(1, degree+1):
+        phi_x = np.c_[phi_x, np.power(x, deg)]
+    # Square root of absolute value
+    phi_x = np.c_[phi_x, np.sqrt(np.abs(x))]
+    # Vectorizing the computing
+    i, j = np.triu_indices(x.shape[1], 1)
+    phi_x = np.c_[phi_x, x[:, i] * x[:, j]]
+    return phi_x
+
+
+
+""" Prediction tools """
 
 def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data matrix"""
@@ -39,6 +168,31 @@ def predict_labels_log_reg(weights, data):
     y_pred[np.where(y_pred >= 0.5)] = 1
     return y_pred
 
+""" Metrics tools """
+#Ref: https://en.wikipedia.org/wiki/Precision_and_recall
+def metrics(y_test, y_pred):
+    tp, fp, tn, fn = 0, 0, 0, 0
+    for i in range(len(y_pred)):
+        if (y_pred[i] == 1):
+            if (y_test[i] == 1):
+                tp += 1
+            else:
+                fp += 1
+        else:
+            if (y_test[i] == 1):
+                fn += 1
+            else:
+                tn += 1
+    #precision = tp/(tp+fp)
+    #recall = tp/(tp+fn)
+    accuracy = (tp+tn)/(tp+tn+fp+fn)
+    f1_score = 2*tp/(2*tp+tn+fp+fn)
+
+    return accuracy, f1_score#, precision, recall
+
+
+""" Exporting the results tool """
+
 def create_csv_submission(ids, y_pred, name):
     """
     Creates an output file in csv format for submission to kaggle
@@ -51,85 +205,4 @@ def create_csv_submission(ids, y_pred, name):
         writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
-            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
-            
-            
-def preprocessing(y,tx, train = True, columns_to_keep = None):    
-    # split data into 4 different categories
-    
-    def clean_xi(xi, train, columns_to_keep = None):
-        """ Function to clean in depth each categorical dataset """
-        x = copy.deepcopy(xi)
-        # set the -999 to the median of the column
-        x = np.where(x==-999, np.nan, x)
-        col_median = np.nanmedian(x, axis=0)
-        inds_nan = np.where(np.isnan(x))
-        x[inds_nan] = np.take(col_median, inds_nan[1])
-        
-        if train:
-            # remove the nan and even columns
-            nan_columns = np.all(np.isnan(x), axis=0)
-            #print("nan_columns :: ",nan_columns)
-            even_columns = np.all(x == x[0,:], axis = 0)
-            #print("even_columns :: ",even_columns)
-            columns_to_remove = nan_columns | even_columns
-            #print("columns_to_remove :: ",columns_to_remove)
-            x = x[:,~columns_to_remove]
-            """
-            # normalize the dataset
-            minmax = implementations.dataset_minmax(x)
-            implementations.normalize_dataset(x, minmax)
-            """            
-            x,_,_ = implementations.standardize(x)
-            # Insert a 1 column for non null intersection
-            x = np.insert(x, 0, 1, axis=1)
-            return x, columns_to_remove
-        else:            
-            x = x[:,columns_to_keep]
-            """            
-            # normalize the dataset
-            minmax = implementations.dataset_minmax(x)
-            implementations.normalize_dataset(x, minmax)
-            """
-            x,_,_ = implementations.standardize(x)
-            # Insert a 1 column for non null intersection
-            x = np.insert(x, 0, 1, axis=1)
-            return x
-            
-    
-    if train:
-        Indices0 = tx[:,22]==0
-        y0, x0 = y[Indices0], tx[np.where(Indices0)]
-
-        Indices1 = tx[:,22]==1
-        y1, x1 = y[Indices1], tx[np.where(Indices1)]
-        
-
-        Indices2 = np.logical_or(tx[:,22]==2, tx[:,22]==3)
-        y2, x2 = y[Indices2], tx[np.where(Indices2)]
-
-        #Indices3 = tx[:,22]==3
-        #y3, x3 = y[Indices3], tx[np.where(Indices3)]
-        
-        x0, columns_to_remove0 = clean_xi(x0, train)
-        x1, columns_to_remove1 = clean_xi(x1, train)
-        x2, columns_to_remove2 = clean_xi(x2, train)
-        #x3, columns_to_remove3 = clean_xi(x3, train)
-        return [(y0,x0),(y1,x1),(y2,x2), (~columns_to_remove0,~columns_to_remove1,~columns_to_remove2)]
-    else:
-        i0, i1, i2 = columns_to_keep
-        
-        rows0 = tx[:,22]==0
-        x0 = tx[np.where(rows0)]
-        rows1 = tx[:,22]==1
-        x1 = tx[np.where(rows1)]
-        rows2 = np.logical_or(tx[:,22]==2, tx[:,22]==3)
-        x2 = tx[np.where(rows2)]
-        
-        x0 = clean_xi(x0, train, i0)
-        x1 = clean_xi(x1, train, i1)
-        x2 = clean_xi(x2, train, i2)
-        return [x0, x1, x2, rows0, rows1, rows2]
-    
-    
-    
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})    
